2fb4fd3fd454b0dbc33cdaf8bb0b8c460f261050
Junu <junustha4@gmail.com>
2022-03-23 17:06:55 +0545

    first commit

---
+++ patternexample/Sample1.java
@@ -0,0 +1,15 @@
+package com.junu.patternexample;
+
+public class Sample1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+    }
+
+}
